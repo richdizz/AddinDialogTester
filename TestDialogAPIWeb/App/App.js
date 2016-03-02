a7caf16b1f007a66a489c5806b77e468d3ccae4e
Richard diZerega <richdizz@outlook.com>
2016-03-01 20:42:29 -0600

    initial checkin

---
+++ TestDialogAPIWeb/App/App.js
@@ -0,0 +1,37 @@
+﻿/* Common app functionality */
+
+var app = (function () {
+    "use strict";
+
+    var app = {};
+
+    app.clientId = "8e6a163d-a531-49cf-b666-d9ba1d920bca";
+    app.tenant = "rzdemos.com";
+    app.redirectUri = "https://localhost:44300/app/home/auth.html";
+
+    // Common initialization function (to be called from each page)
+    app.initialize = function () {
+        $('body').append(
+            '<div id="notification-message">' +
+                '<div class="padding">' +
+                    '<div id="notification-message-close"></div>' +
+                    '<div id="notification-message-header"></div>' +
+                    '<div id="notification-message-body"></div>' +
+                '</div>' +
+            '</div>');
+
+        $('#notification-message-close').click(function () {
+            $('#notification-message').hide();
+        });
+
+
+        // After initialization, expose a common notification function
+        app.showNotification = function (header, text) {
+            $('#notification-message-header').text(header);
+            $('#notification-message-body').text(text);
+            $('#notification-message').slideDown('fast');
+        };
+    };
+
+    return app;
+})();
---
+++ TestDialogAPIWeb/App/Home/Home.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $("#btnDialog").click(function () {
+                var url = "https://localhost:44300/app/home/auth.html";
+
+                Office.context.ui.displayDialogAsync(url, { height: 40, width: 40, requireHTTPS: true }, function (result) {
+                    var _dlg = result.value;
+                    _dlg.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, processMessage);
+                });
+            });
+        });
+    };
+
+    function processMessage(arg) {
+        if (arg) {
+            var token = JSON.parse(arg.message);
+            $.ajax({
+                url: "https://graph.microsoft.com/v1.0/me/contacts",
+                headers: {
+                    "accept": "application/json",
+                    "Authorization": "Bearer " + token.accessToken
+                },
+                success: function (data) {
+                    $("#results").html("success");
+                },
+                error: function (err) {
+                    $("#results").html("error");
+                }
+            });
+        }
+    }
+})();
